101f1243b958c1f61a50a1a88f2ddb47b881136f
ankit90s <ak7591527@gmail.com>
2023-08-31 16:26:17 +0530

    updates

--- 6-themes.sh
+++ 6-themes.sh
@@ -28,7 +28,10 @@ apt install -y materia-gtk-theme
 
 # mint themes
 echo -e "\e[1;32m Installing linux mint themes \e[0m"
-# apt install -y /home/$username/debian-de/deb/*.deb
+wget http://packages.linuxmint.com/pool/main/m/mint-x-icons/mint-x-icons_1.6.5_all.deb
+wget http://packages.linuxmint.com/pool/main/m/mint-themes/mint-themes_2.1.5_all.deb
+apt install -y ./mint-x-icons_1.6.5_all.deb
+apt install -y ./mint-themes_2.1.5_all.deb
 
 # Colloid theme
 # git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
