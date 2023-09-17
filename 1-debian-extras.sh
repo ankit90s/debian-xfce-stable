d04ce01ebad28572e431c89095a41e91ba39daf6
ankit90s <ak7591527@gmail.com>
2023-09-17 22:09:53 +0530

    updates

--- 1-debian-extras.sh
+++ 1-debian-extras.sh
@@ -73,9 +73,6 @@ apt install -y mesa-vulkan-drivers libvulkan1 vulkan-tools vulkan-validationlaye
 echo -e "\e[1;32m Installing build tools \e[0m"
 apt install -y build-essential linux-headers-$(uname -r) intel-microcode
 
-# remove old drivers:
-apt purge -y xserver-xorg-video-intel
-
 echo 
 echo -e "\e[1;32m Rebooting please wait \e[0m"
 sleep 5s
