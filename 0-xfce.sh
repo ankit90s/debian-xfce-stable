76f8dab7d7576c0444003805815126a2c70fc21d
ankit90s <117509009+ankit90s@users.noreply.github.com>
2023-10-15 19:51:56 +0530

    Update 0-xfce.sh

--- 0-xfce.sh
+++ 0-xfce.sh
@@ -73,6 +73,7 @@ apt install -y conky neofetch htop
 echo -e "\e[1;32m Installed firefox-esr or chromium \e[0m"
  wget -O FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US" 
  tar -xf firefoxsetup.tar.bz2 --directory /opt
+ cp settings/firefox-stable.desktop /usr/share/applications
 
 echo -e "\e[1;32m Installed Evince document viewer \e[0m"
 apt install -y evince
